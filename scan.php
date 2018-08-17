c54202d990f97e8f48765f7d0feb45c4dcac62cd
svetasemashko <svetasemashko@gmail.com>
2018-08-17 11:15:47 +0300

    sketch

---
+++ scan.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
+    <title>Scanning</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+<div>
+    <div class="wrapper">
+
+        <div class="header">
+            <div class="logo">
+    <!--            TODO Уменьшить логотип до 66*66px -->
+                <img src="images/logo_box.png" alt="logotype" title="Scan more">
+            </div>
+            <div class="logo-name">Best Scan Company</div>
+            <div class="corner">
+                <button class="exit-button"></button>
+            </div>
+            <div class="version-name">Version 1.2</div>
+        </div>
+
+        <div class="wrapper-inner">
+            <div class="sidebar">
+                sidebar
+            </div>
+
+            <div class="content">
+                content
+            </div>
+        </div>
+
+
+
+    </div>
+
+
+
+    <footer>
+        <div id="footer">
+            <div class="social">
+
+            </div>
+            <p class="copyright">© 2018 Sveta Nazarova</p>
+        </div>
+    </footer>
+</div>
+
+</body>
+</html>
